03569af02e30ddccafb6f6f9fce6bcc71d3fdbc2
Shrijay <shrimankar16102004@gmail.com>
2025-09-14 16:57:29 +0530

    Update project with latest code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { ThemeToggle } from "@/components/ThemeToggle";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function HomePage(){
   return <>
     <SignInButton/>
     <UserButton/>
+    <ThemeToggle/>
   </>
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,6 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { env } from "./data/env/server";
 
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
@@ -6,7 +8,28 @@ const isPublicRoute = createRouteMatcher([
   "/api/webhooks(.*)",
 ]);
 
+const aj = arcjet({
+  key: env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+});
+
 export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    return new Response(null, { status: 403 });
+  }
 
   if (!isPublicRoute(req)) {
     await auth.protect();
